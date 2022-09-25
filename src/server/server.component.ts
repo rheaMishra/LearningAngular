6d4e04db2439a21c70a4961f907f693c0ed4fd91
Riya Mishra <rmishra50.be18@gmail.com>
2022-09-25 11:13:42 +0530

    added string interpolation and property binding

--- src/server/server.component.ts
+++ src/server/server.component.ts
@@ -3,8 +3,21 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent{
+    allowNewServer=false;
+    ServerID = 10;
+    ServerStatus = 'online';
+    getServerStatus(){
+        return this.ServerStatus;
+    }
+    constructor() {
+        setTimeout(() =>{
+            this.allowNewServer = true;
+        },2000);
+
+    }
 
 }
